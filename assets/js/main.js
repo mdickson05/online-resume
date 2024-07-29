baae19978ec6601d9e725f62aff870396c9464d0
mdickson05 <124563538+mdickson05@users.noreply.github.com>
2024-07-29 14:32:57 +0800

    FEAT: Configured Mixitup to allow for project filtering in work section

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Mixitup Filter
+let mixerProjects = mixitup('.project__container', {
+    selectors: {
+        target: '.project__item'
+    },
+    animation: {
+        duration: 300
+    }
+});
